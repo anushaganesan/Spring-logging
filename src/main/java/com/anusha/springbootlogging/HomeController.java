4ec8c69d10ef9e841e7c1f9c3e650f653e597d3c
Anusha <anusha.ganesan@outlook.com>
2020-03-05 21:38:16 -0600

    First Commit

---
+++ src/main/java/com/anusha/springbootlogging/HomeController.java
@@ -0,0 +1,24 @@
+package com.anusha.springbootlogging;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HomeController {
+	
+	Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+   @GetMapping
+    public String home(){
+
+	   logger.trace("All good");
+        return "Hello Logging";
+    }
+
+
+}
